17c7377615ec3a24504a87eaa98b88dd4749827c
felix <fungzhanyang@163.com>
2019-06-08 16:31:19 +0800

    add swiper

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,16 +1,19 @@
 <template>
   <div>
     <home-header></home-header>
+    <home-swiper></home-swiper>
     <router-link to="/list">go list</router-link>
   </div>
 </template>
 
 <script>
 import HomeHeader from './components/Header'
+import HomeSwiper from './components/swiper'
 export default {
   name: 'Home',
   components: {
-    HomeHeader
+	HomeHeader,
+	HomeSwiper
   }
 }
 </script>
---
+++ src/pages/home/components/swiper.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="swiper">
+    <swiper :options="swiperOption">
+      <swiper-slide v-for="item of swiperList" :key="item.id">
+        <img class="swiper-img" :src="item.imgUrl">
+      </swiper-slide>
+      <div class="swiper-pagination" slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomeSwiper",
+  data() {
+    return {
+      swiperOption: {
+		  pagination: '.swiper-pagination',
+		  loop: true
+	  },
+	  swiperList: [
+		  {
+			  id: 1,
+			  imgUrl: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20195/b0732b5a58a2c613f6202beb35c571d3.jpg_750x200_c6a2ebe2.jpg'
+		  },
+		  {
+			  id: 2,
+			  imgUrl: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20193/d7bbc21db442366a882e04ddc984669a.jpg_750x200_85e640d9.jpg'
+		  }
+	  ]
+    };
+  }
+};
+</script>
+
+<style lang="stylus" scoped>
+.swiper >>> .swiper-pagination-bullet-active
+	background #fff
+.swiper
+	overflow hidden
+	width 100%
+	height 0
+	padding-bottom 26.66%
+	background #ccc
+	.swiper-img
+		width 100%
+</style>
