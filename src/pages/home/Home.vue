dda91d91df3a3bf18de3cabd8f5e135436dc3226
felix <fungzhanyang@163.com>
2019-06-15 23:24:36 +0800

    update

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -15,42 +15,55 @@ import HomeIcons from './components/Icons'
 import HomeRecommend from './components/Recommend'
 import HomeWeekend from './components/Weekend'
 import axios from 'axios'
+import {mapState} from 'vuex'
 export default {
-  name: 'Home',
-  components: {
-	HomeHeader,
-	HomeSwiper,
-	HomeIcons,
-	HomeRecommend,
-	HomeWeekend,
-  },
-  data(){
-	  return {
-		  swiperList: [],
-		  iconList: [],
-		  recommendList: [],
-		  weekendList: [],
-	  }
-  },
-  methods:{
-	  getHomeInfo(){
-		  axios.get('/api/index.json')
-		  	.then(this.getHomeInfoSucc)
-	  },
-	  getHomeInfoSucc(res){
-		  res = res.data
-		  if(res.ret&&res.data){
-			  const data = res.data
-			  this.swiperList = data.swiperList
-			  this.iconList = data.iconList
-			  this.recommendList = data.recommendList
-			  this.weekendList = data.weekendList
-		  }
-	  }
-  },
-  mounted(){
-	  this.getHomeInfo()
-  }
+	name: 'Home',
+	components: {
+		HomeHeader,
+		HomeSwiper,
+		HomeIcons,
+		HomeRecommend,
+		HomeWeekend,
+	},
+	data(){
+		return {
+			lastCity: '',
+			swiperList: [],
+			iconList: [],
+			recommendList: [],
+			weekendList: [],
+		}
+	},
+	methods:{
+		getHomeInfo(){
+			axios.get('/api/index.json?city='+this.city)
+				.then(this.getHomeInfoSucc)
+		},
+		getHomeInfoSucc(res){
+			res = res.data
+			if(res.ret&&res.data){
+				const data = res.data
+				this.swiperList = data.swiperList
+				this.iconList = data.iconList
+				this.recommendList = data.recommendList
+				this.weekendList = data.weekendList
+			}
+		}
+	},
+	computed: {
+		...mapState(['city'])
+	},
+	mounted(){
+		this.lastCity = this.city
+		this.getHomeInfo()
+	},
+	activated(){//页面重新显示时会调用(keep-alive新增)
+		if(this.lastCity!==this.city){
+			this.lastCity = this.city
+			this.getHomeInfo()
+		}
+		console.log('a')
+	}
 }
 </script>
 
